f9c5f14dbfa0c00117de2e8114db26e9fc11cf57
HiroUrata <1712026@i.softbank.jp>
2021-11-06 23:41:41 +0900

    Initial Commit

---
+++ RakuBookAPILessonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RakuBookAPILessonApp
+//
+//  Created by UrataHiroki on 2021/11/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
