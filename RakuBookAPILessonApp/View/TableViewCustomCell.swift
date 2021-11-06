ee2589aa9dafdb7128f2f1e5992bb92f9dd8eb7c
HiroUrata <1712026@i.softbank.jp>
2021-11-06 23:54:06 +0900

    TableViewCustomCell 作成　

---
+++ RakuBookAPILessonApp/View/TableViewCustomCell.swift
@@ -0,0 +1,43 @@
+//
+//  TableViewCustomCell.swift
+//  RakuBookAPILessonApp
+//
+//  Created by UrataHiroki on 2021/11/06.
+//
+
+import UIKit
+
+class TableViewCustomCell: UITableViewCell {
+
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var bookTitleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var itemPriceLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        bookImageView.image = UIImage(named: "")
+        bookTitleLabel.text = ""
+        authorLabel.text = ""
+        itemPriceLabel.text = ""
+        
+    }
+}
